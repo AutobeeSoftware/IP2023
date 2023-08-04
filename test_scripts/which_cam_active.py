61b9f7aa771e51c8dcdfe32c138541f9a5c94988
Emir Yasin Sağlam <emir_sglm1@hotmail.com>
2023-08-04 13:43:13 +0300

    Update which_cam_active.py

--- test_scripts/which_cam_active.py
+++ test_scripts/which_cam_active.py
@@ -6,31 +6,82 @@ from CSI_Camera import CSI_Camera
 import cv2
 
 cam1 = CSI_Camera() # primer camera
+cam1_is_open = False
+
 cam2 = CSI_Camera()# seconder camera
-id_ = {cam1 : "0001", cam2: "0002"}
+cam2_is_open = False
 
 
-cam1.open(CSI_Camera.gstreamer_pipeline(0))
-cam2.open(CSI_Camera.gstreamer_pipeline(1))
+try:
+    cam1.open(CSI_Camera.gstreamer_pipeline(0))
+    cam1_is_open = True
 
-while True:
+except:
+    print("Cam1 is failed to open")
+    pass
 
-    ret1, frame1 = cam1.read()
-    ret2, frame2 = cam2.read()
+try:
+    cam2.open(CSI_Camera.gstreamer_pipeline(1))
+    cam2_is_open = True
 
-    if ret1 is False and ret2 is False: # if both cam are not active
-        print(f"{id_[cam1]}\n{id_[cam2]}")
+except:
+    print("Cam2 is failed to open")
+    pass
+
+
+
+while cam1_is_open and cam2_is_open :
+
+
+    if cam1_is_open is False and cam2_is_open is False: 
+        print("No camera to read")
         break
-    elif ret1 is not True: # if only cam1 is not active
-        print(id_[cam1])
-        break
-    elif ret2 is not True: # if only cam2 is not active
-        print(id_[cam2])
-        break
+    elif cam1_is_open is True and cam2_is_open is False: 
+        ret1, frame1 = cam1.read()
+        if ret1 == False:
+            print("cam1 is opened but couldnt read")
+            break
+        else:
+            cv2.imshow("cam1", frame1)
+            cv2.waitKey(3)
 
-    cv2.imshow("cam1", frame1)
-    cv2.imshow("cam2", frame2)
-    cv2.waitKey(30)
+    elif cam1_is_open is False and cam2_is_open is True: 
+        ret2, frame2 = cam2.read()
+        if ret2 == False:
+            print("cam2 is opened but couldnt read")
+            break
+        else:
+            cv2.imshow("cam2", frame2)
+            cv2.waitKey(3)
+
+    else:
+        ret1, frame1 = cam1.read()
+        ret2, frame2 = cam2.read()
+
+        if ret1 == False and ret2 == False:
+            print("cam1 and cam2 are opened but couldnt read")
+            break
+
+        elif ret1 == True and ret2 == False:
+            print("cam2 is opened but couldnt read")
+            print("cam1 is working")
+            cv2.imshow("cam1", frame1)
+            cv2.waitKey(3)
+
+        elif ret1 == False and ret2 == True:
+            print("cam1 is opened but couldnt read")
+            print("cam2 is working")
+            cv2.imshow("cam2", frame2)
+            cv2.waitKey(3)
+
+        else:
+            cv2.imshow("cam1", frame1)
+            cv2.imshow("cam2", frame2)
+
+            cv2.waitKey(30)
+
+
+
 
 cam1.stop()
 cam2.stop()
